8957ae84b6e13f8826be60c09ab4bd21187015aa
Stephen <stephenk9966@gmail.com>
2018-05-20 12:53:14 +0200

    ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBB-Id9EBdB2gnTprmlzmZHv2XxJ2ZxnxU",
+    authDomain: "d3app-865c2.firebaseapp.com",
+    databaseURL: "https://d3app-865c2.firebaseio.com",
+    projectId: "d3app-865c2",
+    storageBucket: "",
+    messagingSenderId: "264459064376"
+  }
 };
